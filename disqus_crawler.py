ca916381102f2328c6e24c652a69f2cea27b3ded
felix <felix.s@posteo.de>
2017-09-21 15:47:02 +0200

    filled out and tested the user class

--- disqus_crawler.py
+++ disqus_crawler.py
@@ -1,23 +1,44 @@
+# https://github.com/disqus/disqus-python/issues/34
+# pip install --upgrade https://github.com/disqus/disqus-python/archive/master.zip
 from disqusapi import DisqusAPI
-import config
+from config import *
+
+disqus = DisqusAPI(SECRET_KEY, PUBLIC_KEY)
+
+def softget(dictionary, key):
+    if key in dictionary:
+        return dictionary[key]
 
 class User():
-    def __init__(self, username, num_followers, display_name, joined_at, user_since,
-                 about, connections, num_posts, reputation, num_likes_received,
-                 owns_their_own_forum, disqus_unique_id, num_following):
+    def __init__(self, ID, username=None, about=None, name=None, disable3rdPartyTrackers=None,
+                 isPowerContributor=None, joinedAt=None, rep=None, profileUrl=None, url=None,
+                 reputation=None, location=None, isPrivate=None, signedUrl=None, isPrimary=None,
+                 isAnonymous=None, reputationLabel=None, avatar=None, numFollowers=None,
+                 numFollowing=None, numForumsFollowing=None, numLikesReceived=None,
+                 numPosts=None):
         self.username = username
-        self.num_followers = num_followers
-        self.display_name = display_name
-        self.joined_at = joined_at
-        self.user_since = user_since
         self.about = about
-        self.connections = connections
-        self.num_posts = num_posts
+        self.name = name
+        self.disable3rdPartyTrackers = disable3rdPartyTrackers
+        self.isPowerContributor = isPowerContributor
+        self.joinedAt = joinedAt
+        self.rep = rep
+        self.profileUrl = profileUrl
+        self.url = url
         self.reputation = reputation
-        self.num_likes_received = num_likes_received
-        self.owns_their_own_forum = owns_their_own_forum
-        self.disqus_unique_id = disqus_unique_id
-        self.num_following = num_following
+        self.location = location
+        self.isPrivate = isPrivate
+        self.signedUrl = signedUrl
+        self.isPrimary = isPrimary
+        self.isAnonymous = isAnonymous
+        self.id = ID   # capitals as Python protected
+        self.reputationLabel = reputationLabel
+        self.avatar = avatar
+        self.numFollowers = numFollowers  # this and following rows need to come from a different API call
+        self.numFollowing = numFollowing
+        self.numForumsFollowing = numForumsFollowing
+        self.numLikesReceived = numLikesReceived
+        self.numPost = numPosts
 
     def add_to_sql(self):
         """
@@ -50,10 +71,20 @@ class WebsiteForum():
         """
         pass
 
+
 class ObjectPopulator():
-    def __init__(self, users_list, website_list):
+    def __init__(self, users_list, website_list, forum_name):
         self.users_list = users_list
         self.website_list = website_list
+        self.forum_name = forum_name
+
+    @property
+    def get_users_list(forum_name):
+        userslist = disqus.get('forums.listUsers', method='GET', forum=forum_name, limit=100).response
+        assert isinstance(userslist, list)
+        return userslist
+
+    # https://stackoverflow.com/questions/30420621/python-creating-object-instances-in-a-loop-with-independent-handling
 
     def add_user(self):
         """
@@ -87,6 +118,7 @@ class ObjectPopulator():
 
 class Crawler(ObjectPopulator):
     def __init__(self):
+        super().__init__()
         self.qualified_user_list = []
         self.qualified_website_list = []
 
@@ -102,10 +134,32 @@ class Crawler(ObjectPopulator):
     def remove_user(self):
         pass
 
+# create one test instance :
 
-
-
-
-
+user_one = User(
+    username = softget(u,'username'),
+    about = softget(u,'about'),
+    name = softget(u,'name'),
+    disable3rdPartyTrackers = softget(u,'disable3rdPartyTrackers'),
+    isPowerContributor = softget(u,'isPowerContributor'),
+    joinedAt = softget(u,'joinedAt'),
+    rep = softget(u,'rep'),
+    profileUrl = softget(u,'profileUrl'),
+    url = softget(u,'url'),
+    reputation = softget(u,'reputation'),
+    location = softget(u,'location'),
+    isPrivate = softget(u,'isPrivate'),
+    signedUrl = softget(u,'signedUrl'),
+    isPrimary = softget(u,'isPrimary'),
+    isAnonymous = softget(u,'isAnonymous'),
+    ID = softget(u,'id'),
+    reputationLabel = softget(u,'reputationLabel'),
+    avatar = softget(u,'avatar'),
+    numFollowers = softget(v,'numFollowers'),
+    numFollowing = softget(v,'numFollowing'),
+    numForumsFollowing = softget(v,'numForumsFollowing'),
+    numLikesReceived = softget(v,'numLikesReceived'),
+    numPosts = softget(v,'numPosts'),
+)
 
 
